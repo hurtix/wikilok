3aaa5e25957278179cface0b79a275ade7a4b4a8
hurtix <hurtix@gmail.com>
2025-04-03 08:41:34 -0500

    Create tpl-landing

---
+++ theme/page-templates/tpl-landing.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Template Name: UI Landing
+ *
+ * This is the template that displays all pages by default. Please note that
+ * this is the WordPress construct of pages: specifically, posts with a post
+ * type of `page`.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package gorilabs
+ */
+
+get_header();
+?>
+
+<section id="primary">
+	<main id="main" class="text-xl">
+ç
+	</main><!-- #main -->
+</section><!-- #primary -->
+
+
+
+<?php
+get_footer();
